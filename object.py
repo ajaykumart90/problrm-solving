7b1ccbc8b267e817b5e67c5256354ee6a39d61c6
Ajay Kumar <ajayajay80554@gmail.com>
2026-09-05 14:55:33 +0530

    Initial commit

---
+++ object.py
@@ -0,0 +1,39 @@
+# num1=10
+# print(num1)
+# print(id(num1))
+# print(type(num1))
+
+
+
+# comp1=23+78
+# print(comp1)
+# print(id(comp1))
+# print(type(comp1))
+
+# print(print)
+# print(type(print))
+# print(id(print))
+
+#if an object changes if you make any edit to the object then it is called immutable object and if an object does not change if you make any edit to the object then it is called mutable object.
+# var1= 10
+# var1+= 5
+# print(var1)
+# print(id(var1))
+# print(type(var1))
+# flt1=2.5
+# print(flt1)
+# print(id(flt1))
+# print(type(flt1))
+
+set1= {1,2,3,4,5}
+print(set1) 
+print(id(set1))
+set1!= {1,2,3,4,5}
+print(id(set1))
+
+dict1= {'name':'Ajay', 'age': 25, 'qualification': 'B.Tech'}
+dict2= {'name':'ganesh', 'age': 26, 'qualification': 'B.Tech'}
+print(dict1)  
+dict1!=dict2  
+print(id(dict1))
+print(type(dict1))
