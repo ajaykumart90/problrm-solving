48be4d3072c51c3a4c93aa9acc0a6b1bc350c2c8
Ajay Kumar <ajayajay80554@gmail.com>
2026-09-05 15:12:03 +0530

    Added keywords.py

---
+++ keywords.py
@@ -0,0 +1,27 @@
+# def details(name, Course, mode):
+#     return f'Student name is {name},course is {Course} and mode of teaching {mode}'
+
+# print(details('Ajay','Data science','Offline'))
+
+# print(details( Course='java full stack',mode='Online', name =' Ganesh'))
+
+
+#waf to calculate age of a person
+# def shoppingbill(item, price, quantity):
+#     total = price*quantity
+#     return f'Total bill of product {item} is : {total}'
+# print(shoppingbill(item='Chocolate',price= 97, quantity=3))
+
+
+#wap to withdraw amount if cardnum = 123456789 and pin = 4157
+# def attemp(name, empid,present_days, Working_days):
+#     Attendance= (present_days/ Working_days)*100
+
+#     return f'The Employee name is {name}, Employee id {empid}, Present days {present_days}'
+
+
+# print(attemp( name = 'Ajay', empid= 45, present_days= 48, Working_days= 50))
+#wap to calculate course fee based on type of student 
+# if student in colege 10%
+# if student unemployedd 5%
+#if studnet employee 2%
